af9eec1a883771ecff6b43d9dc4a321ff10d3db5
Mariano Ruiz <marianonicolasruiz@gmail.com>
2022-05-27 12:44:18 -0300

    Se agrega proyecto

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Farmacia
+{
+    class Program{
+        public static void Main(string[] args){
+            Console.WriteLine("Hello World!");
+            // Buendia
+        }
+    }
+}
