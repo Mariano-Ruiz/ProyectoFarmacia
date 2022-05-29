015202b0945cbdc43e98d4a1c4982fc7f85f76f2
Mariano Ruiz <marianonicolasruiz@gmail.com>
2022-05-29 12:43:33 -0300

    Agrego mas clases

--- Clases.cs
+++ Clases.cs
@@ -1,11 +1,34 @@
+using System.Collections;
+
 namespace ProjectoFarmacia
 {
     public class Farmacia
     {
-        
+        Array[Empleado];
+        Array[Venta];
+        Array[Medicamento];
     }
 
     public class Venta{
+        private string obraSocial;
+        private int numTicket, codVendedor;
+        private Medicamento medicamento;
+
+        public Venta(Medicamento medicamento, strig obraSocial, int numTicket, int codVendedor){
+
+        }
+        public Medicamento medicamento{
+            get; set;
+        }
+        public strig ObraSocial{
+            get; set;
+        }
+        public int NumTicket{
+            get; set;
+        }
+        public int CodVendedor{
+            get; set;
+        }
 
     }
 
@@ -30,19 +53,23 @@ namespace ProjectoFarmacia
     }
 
     public class Empleado{
-        private string nombre;
-        private string dni;
+        private string nombre, dni;
+        private int codVendedor;
         private DateTime fechaNacimiento;
 
-        public Empleado(string nombre, string dni, DateTime fechaNacimiento){
+        public Empleado(string nombre, string dni, int codVendedor, DateTime fechaNacimiento){
             Nombre = nombre;
             Dni = dni;
+            CodVendedor = codVendedor
             FechaNacimiento = fechaNacimiento;
         }
         public string Nombre{
             get; set;
         }
         public string Dni{
+            get; set;
+        }
+        public int CodVendedor{
             get; set;
         }
         public DateTime FechaNacimiento{
